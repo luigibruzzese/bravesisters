ef0a8e7dfda5dcc348484d7285e7f2c0f1c92097
luigibruzzese <luigi.bruzzese@mail.polimi.it>
2024-05-29 23:53:00 +0200

    GeneralInfoComponent setup

---
+++ components/GeneralInfoComponent.vue
@@ -0,0 +1,62 @@
+<script setup lang="js">
+    defineProps({
+        id: Number,
+        fullName: String,
+        role: String,
+        shortPresentation: String
+    })
+</script>
+
+<template>
+    <div class="generalInfo">
+        <NuxtLink :href="'/people-' + (parseInt(id)-1).toString()">
+            <img alt="Left arrow" class="arrow" src="~/assets/icons/left-arrow.png"/>
+        </NuxtLink>
+        <div>
+            <img class="portrait" :src="'_nuxt/assets/img/people/' + id + '.png'"/>
+            <div>
+                <h2>{{ fullName }}</h2>
+                <h4>{{ role }}</h4>
+                <p>{{ shortPresentation }}</p>
+            </div>
+        </div>
+        <NuxtLink :href="'/people-' + (parseInt(id)+1).toString()">
+            <img alt="Right arrow" class="arrow" src="~/assets/icons/right-arrow.png"/>
+        </NuxtLink>
+    </div>
+</template>
+
+<style scoped>
+    .arrow {
+    //border-left: #b7403c solid 2px; width: 35px; width: 25px;
+        width: 60px;
+    }
+
+    .generalInfo, .generalInfo > div {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        text-align: center;
+        margin: 5%;
+    }
+
+
+    .portrait {
+        width: 300px;
+        margin: 5%;
+    }
+
+    @media(max-width: 800px) {
+        .generalInfo > div {
+            display: initial;
+        }
+    }
+    @media(max-width: 600px) {
+        .portrait {
+            width: 200px;
+        }
+        .arrow {
+            width: 50px;
+        }
+    }
+</style>
---
+++ pages/people-[id].vue
@@ -0,0 +1,24 @@
+<script setup lang="js">
+  const route = useRoute()
+
+  const name = "luigi bruzzese"
+  const role = "helper"
+  const shortPresentation = "This is just a try... aaaa aaaaaa aa aaaaaaaaa aaaaa aaaaaa "
+</script>
+
+<template>
+
+    <GeneralInfoComponent
+            :id = "route.params.id"
+            :full-name = "name"
+            :role = "role"
+            :short-presentation = "shortPresentation"
+
+    >
+
+    </GeneralInfoComponent>
+</template>
+
+<style scoped>
+
+</style>
