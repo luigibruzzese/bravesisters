ba96bc6126af8fdd711edeb5a4c5358fe8cb708a
Aleksandro Aliaj <aleksandroaliaj19@gmail.com>
2024-06-03 18:10:30 +0200

    updates on people-[id] (#8)

--- stores/people.ts
+++ stores/people.ts
@@ -1,28 +1,28 @@
-import {useFetch} from "#app";
-import init from "@parcel/watcher-wasm";
-import type {integer} from "vscode-languageserver-types";
+import { defineStore } from 'pinia';
+import { ref, reactive } from 'vue';
+import { useFetch } from '#app';
 
 interface Person {
-    id: number,
-    name:string,
-    surname:string,
-    role: string,
-    description: string
+  id: number;
+  name: string;
+  surname: string;
+  role: string;
+  description: string;
 }
 
-export const usePeopleStore = defineStore('person',()=>{
-    const people = reactive([] as Person[])
-    async function init() {
-        const {data} = await useFetch<any[]>('/api/people')
-        const list = data.value
-        if(list!=null) {
-            people.splice(0,people.length)  //Clear the list
-            people.push(...list)
-        }
+export const usePeopleStore = defineStore('person', () => {
+  const people = reactive([] as Person[]);
+  
+  async function init() {
+    const { data } = await useFetch<Person[]>('/api/people');
+    const list = data.value;
+    if (list) {
+      people.splice(0, people.length);  // Clear the list
+      people.push(...list);
     }
+  }
 
-    init()
+  init();
 
-    return {people}
-})
-
+  return { people, init };
+});
